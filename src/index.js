692111bebac42002acf4faa33866abaf079b3948
Abdil1810 <dillah190498@gmail.com>
2025-09-15 07:20:48 +0800

    Fix routing for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import './styles/styles.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
